87f71d6e5ca685b25c86564d2ba300be15e9f35b
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2021-03-15 01:24:44 +0530

    Improved the legend

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
 
-    const array = [6, 4, 6, 3, 9, 8, 13, 10, 5, 6, 7];
+    const array = [9, 4, 6, 3, 9, 8, 13, 10, 5, 6, 7];
     visualizer = new PartitionVisualizer(array, width, height);
     setTimeout(() => visualizer.runIteration(), ITERATION_DELAY);
 }
